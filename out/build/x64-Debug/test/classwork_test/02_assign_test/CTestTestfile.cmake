e8cd7edcefe06e3d0ae36638e4f09e8b85f48544
Long Dang <long.dang@g.austincc.edu>
2019-09-16 02:28:04 -0500

    hw 1

---
+++ out/build/x64-Debug/test/classwork_test/02_assign_test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/longd/source/repos/acc-cosc-1337-fall-2019-longdangaustincc/test/classwork_test/02_assign_test
+# Build directory: C:/Users/longd/source/repos/acc-cosc-1337-fall-2019-longdangaustincc/out/build/x64-Debug/test/classwork_test/02_assign_test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(cw_02_tests "C:/Users/longd/source/repos/acc-cosc-1337-fall-2019-longdangaustincc/out/build/x64-Debug/test/classwork_test/02_assign_test/cw_02_tests.exe" "-s")
+set_tests_properties(cw_02_tests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/longd/source/repos/acc-cosc-1337-fall-2019-longdangaustincc/test/classwork_test/02_assign_test/CMakeLists.txt;3;add_test;C:/Users/longd/source/repos/acc-cosc-1337-fall-2019-longdangaustincc/test/classwork_test/02_assign_test/CMakeLists.txt;0;")
---
+++ out/build/x64-Debug/test/homework_test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/longd/source/repos/acc-cosc-1337-fall-2019-longdangaustincc/test/homework_test
+# Build directory: C:/Users/longd/source/repos/acc-cosc-1337-fall-2019-longdangaustincc/out/build/x64-Debug/test/homework_test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

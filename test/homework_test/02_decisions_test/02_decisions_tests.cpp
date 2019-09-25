9358a3f9fc5bdcdc90453fa1bf2eec53233b50e0
Long Dang <long.dang@g.austincc.edu>
2019-09-25 17:37:24 -0500

    hw_02

--- test/homework_test/02_decisions_test/02_decisions_tests.cpp
+++ test/homework_test/02_decisions_test/02_decisions_tests.cpp
@@ -5,3 +5,16 @@ TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Verify get_grade_points function")
+{
+	REQUIRE(get_grade_points("A") == 4);
+	REQUIRE(get_grade_points("B") == 3);
+	REQUIRE(get_grade_points("C") == 2);
+	REQUIRE(get_grade_points("D") == 1);
+	REQUIRE(get_grade_points("F") == 0);
+}
+
+TEST_CASE("Verify calculate_gpa function")
+{
+	REQUIRE(calculate_gpa())
+}

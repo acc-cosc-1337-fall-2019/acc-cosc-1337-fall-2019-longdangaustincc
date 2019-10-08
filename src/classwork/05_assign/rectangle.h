ec2630854498a670d7edfbb41571671cc8c12b2f
Long Dang <long.dang@g.austincc.edu>
2019-10-07 19:00:54 -0500

    ex04_1

--- src/classwork/05_assign/rectangle.h
+++ src/classwork/05_assign/rectangle.h
@@ -3,15 +3,18 @@ Create the interface for a Rectangle class with one constructor that accepts two
 has one public function area that returns the area of the rectangle, and one private function calculate_area
 that calculates area of the rectangle.  The class has 3 int private variables area, width, and height.
 */
-class Rectanglee
+namespace acc
 {
-public:
-	Rectanglee(int w, int h) : width(w), height(h) { calculate_area(); };
-	int get_area() const;
+	class Rectangle
+	{
+	public:
+		Rectangle(int w, int h) : width(w), height(h) { calculate_area(); };
+		int get_area() const;
 
-private:
-	int width;
-	int height;
-	int area;
-	void calculate_area();
-};
+	private:
+		int width;
+		int height;
+		int area;
+		void calculate_area();
+	};
+}

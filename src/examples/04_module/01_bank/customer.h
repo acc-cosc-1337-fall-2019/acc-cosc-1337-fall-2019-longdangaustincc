4b3b72ebfc4cd4ecdef5ddc4a9f3cc6cc5f75a43
Long Dang <long.dang@g.austincc.edu>
2019-10-14 18:22:53 -0500

    ex04_customer

--- src/examples/04_module/01_bank/customer.h
+++ src/examples/04_module/01_bank/customer.h
@@ -1,1 +1,18 @@
 //customer.h
+#include <iostream>
+#include <vector>
+#include "bank_account.h"
+
+using std::ostream;
+using std::vector;
+
+class Customer
+{
+public:
+	void add_account(BankAccount act);
+	friend ostream & operator << (ostream & out, const Customer & c);
+
+private:
+	vector <BankAccount> accounts;
+	int total_balance{ 0 };
+};
--- src/examples/04_module/01_bank/main.cpp
+++ src/examples/04_module/01_bank/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include "atm.h"
 #include <vector>
+#include "atm.h"
+#include "customer.h"
 
 using std::cout;
 using std::cin;
@@ -8,25 +9,12 @@ using std::vector;
 
 int main()
 {
-	BankAccount a;
-	BankAccount b = a; //create a copy of BankAccount a and named b
-	display(a); // class friend function
-	//scan card and enter pin
-	//get record from database into a vector
-	vector <BankAccount> accounts{ BankAccount(500), BankAccount(600), BankAccount(1000) }; //this is just a mimic of the data base
-
-	for (auto act : accounts) // cout vector accounts
-	{
-		cout << act;
-	}
-
-	ATM atm(accounts[0]);
-	atm.deposit(100);
-	atm.withdraw(50);
-	atm.display_balance();
-	cin >> a;
-	cout << a; // overload operator
+	Customer customer;
+	customer.add_account(BankAccount(500));
+	customer.add_account(BankAccount(1500));
+	customer.add_account(BankAccount(5000));
 	
+	cout << customer;
 
 	return 0;
 }

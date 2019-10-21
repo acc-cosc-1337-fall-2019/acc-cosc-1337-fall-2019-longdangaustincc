1d2756c8fa60946ba61e3caea4899cc2627af7ab
Long Dang <long.dang@g.austincc.edu>
2019-10-21 18:11:08 -0500

    Inheritance Polymorphism

--- src/examples/04_module/01_bank/bank_account.h
+++ src/examples/04_module/01_bank/bank_account.h
@@ -3,18 +3,21 @@
 
 #ifndef BANK_ACCOUNT_H //header guards: prevent class redefintion error
 #define BANK_ACCOUNT_H
+
 class  BankAccount
 {
 public:
 	BankAccount();
 	BankAccount(int b) : balance(b) {}; //inline constructor
-	int get_balance() const; //cons makes class members rea only
+	virtual int get_balance() const; //cons makes class members rea only
 	void deposit(int amount);
 	void withdraw(int amount);
 
 	friend void display(const BankAccount & account);
 	friend std::ostream & operator << (std::ostream & out, const BankAccount & b);
 	friend std::istream & operator >> (std::istream & in, BankAccount & b);
+	BankAccount operator+(const BankAccount & b);
+
 protected:
 	int balance;
 	
--- src/examples/04_module/01_bank/main.cpp
+++ src/examples/04_module/01_bank/main.cpp
@@ -3,6 +3,7 @@
 #include "atm.h"
 #include "customer.h"
 #include "checking_account.h"
+#include "savings_account.h"
 
 using std::cout;
 using std::cin;
@@ -10,8 +11,15 @@ using std::vector;
 
 int main()
 {
-	CheckingAccount a(1500);
-	cout << a.get_balance();
+	CheckingAccount checking(1500);
+	cout << "\n Checking get_balance: " << checking.get_balance() << "\n";
+
+	SavingsAccount savings(500);
+	cout << "\n Savings get_balance: " << savings.get_balance() << "\n";
+	
+
+	BankAccount & account = savings;
+	cout << "\n Ref to savings get_balance: " << account.get_balance() << "\n";
 
 	return 0;
 }

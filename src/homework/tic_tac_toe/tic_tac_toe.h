abe08bbeea0a8c39c15d72e448695f9197b8fb86
Long Dang <long.dang@g.austincc.edu>
2019-10-30 19:17:48 -0500

    Assignment 9

--- src/homework/tic_tac_toe/tic_tac_toe.h
+++ src/homework/tic_tac_toe/tic_tac_toe.h
@@ -1,16 +1,12 @@
 #include<string>
 #include<vector>
-
-using std::string; using std::vector;
-
 #include<iostream>
 
+using std::string; using std::vector;
 using std::ostream; using std::istream;
 
 #ifndef TIC_TAC_TOE_H // Header Guard
 #define TIC_TAC_TOE_H
-
-
 
 //h
 /*
@@ -19,6 +15,8 @@ Represents one game of tic tac toe.
 class TicTacToe 
 {
 public:
+	TicTacToe() {};
+	TicTacToe(int size) : pegs(size * size, " ") {};
 	bool game_over();
 	void start_game(string player);
 	void mark_board(int position);
@@ -27,15 +25,17 @@ public:
 	friend ostream& operator<<(ostream& out, const TicTacToe& t);
 	friend istream& operator>>(istream& in, TicTacToe& t);
 
+protected:
+	virtual bool check_column_win();
+	virtual bool check_row_win();
+	virtual bool check_diagonal_win();
+	vector<string> pegs;
+
 private:
 	void set_next_player();
-	bool check_column_win();
-	bool check_row_win();
-	bool check_diagonal_win();
 	void clear_board();
 	bool check_board_full();
 	void set_winner();
-	vector<string> pegs{ 9, " " };//initializer
 	string next_player;
 	string winner;
 	
